0d07200584d3ffaa44259bd2435e6e2598dccd9b
tsudot <kunalkerkar@gmail.com>
2012-03-21 19:58:34 +0530

    Output added

--- PlivoXml.java
+++ PlivoXml.java
@@ -9,12 +9,18 @@ public class PlivoXml {
         ApplicationResponse response = new ApplicationResponse();
 
         Speak speakElement = new Speak();
+        speakElement.setText("Calling from Plivo");
         speakElement.setVoice("MAN");
 
         speakElement.setLanguage("en_US");
         speakElement.setLoop(8);
 
         response.setSpeak(speakElement);
+
+        // Output
+        // <Response>
+        // <Speak loop="8" voice="MAN" language="en_US">Calling from Plivo</Speak>
+        // </Response>
 
     }
 }
