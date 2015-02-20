1623c0dbbb077f8d070f140cf3d709ad53f1c0bd
Ramya <ramya@plivo.com>
2015-02-20 15:18:28 +0530

    forwarding calls example

---
+++ voice/forwardCall.java
@@ -0,0 +1,55 @@
+package plivoexample;
+
+import java.io.IOException;
+
+import com.plivo.helper.exception.PlivoException;
+import com.plivo.helper.xml.elements.Dial;
+import com.plivo.helper.xml.elements.Number;
+import com.plivo.helper.xml.elements.PlivoResponse;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class forwardCall extends HttpServlet {
+    private static final long serialVersionUID = 1L;    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+            throws ServletException, IOException {
+        
+        PlivoResponse response = new PlivoResponse();
+        String fromNumber = req.getParameter("From");
+        
+        Dial dial = new Dial();
+        dial.setCallerId(fromNumber);
+        Number num = new Number("2222222222");
+        
+        try {
+            dial.append(num);
+            response.append(dial);
+            System.out.println(response.toXML());
+            resp.addHeader("Content-Type", "text/xml");
+            resp.getWriter().print(response.toXML());;
+        } catch (PlivoException e) {
+            e.printStackTrace();
+        }       
+    }
+
+    public static void main(String[] args) throws Exception {
+        String port = System.getenv("PORT");
+        if(port==null)
+            port ="8000";
+        Server server = new Server(Integer.valueOf(port));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new forwardCall()),"/forward");
+        server.start();
+        server.join();
+    }
+}
